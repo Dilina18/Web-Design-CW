942966e05ebea8d7460b7372b7666bff4dbfc04d
Dilina18 <randikadilina783@gmail.com>
2022-03-18 12:32:22 +0530

    Web Design-CW
    Completed Project

---
+++ Presentation.js
@@ -0,0 +1,8 @@
+// define auto redirecting time function
+
+function countdown() {
+// set redirect location
+window.location = "Main.html";
+}
+// set countdown
+window.setTimeout("countdown()", 5000);
